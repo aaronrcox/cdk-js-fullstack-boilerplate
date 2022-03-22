cf9026c9c9c0dfeffa3ab78de74be2bc78e08408
Aaron Cox <aaronrcox123@gmail.com>
2022-03-22 14:59:54 +1100

    updated pipeline

--- cdk/src/cdk-frontend-stack.ts
+++ cdk/src/cdk-frontend-stack.ts
@@ -1,4 +1,4 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3, aws_s3_deployment } from 'aws-cdk-lib';
