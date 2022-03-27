88502efbf8b84f1cba960b03c715ff6b42aaf220
Aaron Cox <aaronrcox123@gmail.com>
2022-03-27 14:15:35 +1100

    added apigateway endpoint for lambda func

--- cdk/src/cdk-backend-stack.ts
+++ cdk/src/cdk-backend-stack.ts
@@ -2,6 +2,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkBackendStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,8 +10,12 @@ export class CdkBackendStack extends Stack {
 
         const hello = new lambda.Function(this, 'NestJsBackendApi', {
             runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
-            code: lambda.Code.fromAsset('nest-js-backend-api/dist'),  // code loaded from "lambda" directory
+            code: lambda.Code.fromAsset('nest-js-backend-api/dist'),  // code loaded from this directory
             handler: 'main.handler'                // file is "main", function is "handler"
+        });
+
+        new apigw.LambdaRestApi(this, 'NestJsBackendApiEndpoint', {
+            handler: hello
         });
 
     }
