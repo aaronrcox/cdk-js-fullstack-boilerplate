11c3ea82fa65f3a828ecd5b53be7998e19aa03bd
Aaron Cox <aaronrcox123@gmail.com>
2022-03-29 19:47:56 +1100

    added bastionHost to db stack

--- cdk/src/cdk-dbrds-stack.ts
+++ cdk/src/cdk-dbrds-stack.ts
@@ -6,6 +6,7 @@ import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { IGrantable } from 'aws-cdk-lib/aws-iam';
 import { ServerlessCluster } from 'aws-cdk-lib/aws-rds';
+import { Ec2TaskDefinition } from 'aws-cdk-lib/aws-ecs';
 
 
 export class CdkDbRdsStack extends Stack {
@@ -17,16 +18,35 @@ export class CdkDbRdsStack extends Stack {
 
         const vpc = new ec2.Vpc(this, "AuroraVpc");
 
+        const bastion = new ec2.BastionHostLinux(this, 'BastionHost', {
+            vpc,
+            subnetSelection: {
+                subnetType: ec2.SubnetType.PUBLIC,
+            },
+            blockDevices: [{
+                deviceName: 'EBSBastionHost',
+                volume: ec2.BlockDeviceVolume.ebs(10, {
+                    encrypted: true
+                })
+            }]
+        })
+
+        bastion.connections.allowDefaultPortFromAnyIpv4();
+
         this.cluster = new rds.ServerlessCluster(this, "AuroraDb", {
             engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
             parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, "ParameterGroup", "default.aurora-postgresql10"),
             defaultDatabaseName: "TestDb",
+            enableDataApi: true,
             vpc: vpc,
             scaling: {
                 autoPause: cdk.Duration.minutes(5),
                 minCapacity: cdk.aws_rds.AuroraCapacityUnit.ACU_2
-            }
+            },
+
+
         });
+
     }
 
     grantAccess(granter: IGrantable) {
